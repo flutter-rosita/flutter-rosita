7960273c66ff4fef6acf8e56d10e4d1ed4666615
Egor Dolgov <contact@flutter.su>
2024-05-22 13:11:54 +0300

    [Rosita] Add support CustomClipper<Path>

--- packages/rosita/lib/src/utils/radius.dart
+++ packages/rosita/lib/src/utils/radius.dart
@@ -1,3 +1,9 @@
+// ignore: unnecessary_import
+import 'dart:typed_data';
+
+// ignore: unnecessary_import
+import 'dart:ui';
+
 import 'package:flutter/rendering.dart';
 import 'package:universal_html/html.dart' as html;
 
@@ -44,6 +50,98 @@ class RositaRadiusUtils {
               Radius.circular(size.shortestSide / 2),
             ),
           );
+      }
+    } else if (clipper is CustomClipper<Path>) {
+      final path = clipper.getClip(size);
+
+      if (path is RositaSurfacePath) {
+        final pathRef = path.pathRef;
+        final StringBuffer sb = StringBuffer();
+        final RositaPathRefIterator iter = RositaPathRefIterator(pathRef);
+        final Float32List points = pathRef.points;
+        int verb;
+
+        bool isEven = false;
+
+        final oddList = <String>[];
+
+        Offset previousPoint = Offset.zero;
+
+        while ((verb = iter.nextIndex()) != RositaSPath.kDoneVerb) {
+          final int pIndex = iter.iterIndex;
+
+          switch (verb) {
+            case RositaSPath.kMoveVerb:
+              isEven = !isEven && path.fillType == PathFillType.evenOdd;
+
+              oddList.clear();
+
+              if (isEven) {
+                sb.write('M ${points[pIndex]} ${points[pIndex + 1]} ');
+              } else {
+                previousPoint = Offset(points[pIndex], points[pIndex + 1]);
+                oddList.add('Z ');
+              }
+            case RositaSPath.kLineVerb:
+              if (isEven) {
+                sb.write('L ${points[pIndex + 2]} ${points[pIndex + 3]} ');
+              } else {
+                oddList.add('L ${previousPoint.dx} ${previousPoint.dy} ');
+                previousPoint = Offset(points[pIndex + 2], points[pIndex + 3]);
+              }
+            case RositaSPath.kQuadVerb:
+              if (isEven) {
+                sb.write('Q ${points[pIndex + 2]} ${points[pIndex + 3]},'
+                    ' ${points[pIndex + 4]} ${points[pIndex + 5]}');
+              } else {
+                oddList.add('Q ${points[pIndex + 2]} ${points[pIndex + 3]}, ${previousPoint.dx} ${previousPoint.dy} ');
+                previousPoint = Offset(points[pIndex + 4], points[pIndex + 5]);
+              }
+            case RositaSPath.kConicVerb:
+              // sb.write('Conic(${points[pIndex + 2]}, ${points[pIndex + 3]},'
+              //     ' ${points[pIndex + 3]}, ${points[pIndex + 4]}, w = ${iter.conicWeight})');
+              if (isEven) {
+                sb.write('Q ${points[pIndex + 2]} ${points[pIndex + 3]},'
+                    ' ${points[pIndex + 4]} ${points[pIndex + 5]}'); // [ROSITA] This is not Conic
+              } else {
+                oddList.add('Q ${points[pIndex + 2]} ${points[pIndex + 3]}, ${previousPoint.dx} ${previousPoint.dy} ');
+                previousPoint = Offset(points[pIndex + 4], points[pIndex + 5]);
+              }
+            case RositaSPath.kCubicVerb:
+              if (isEven) {
+                sb.write('C ${points[pIndex + 2]} ${points[pIndex + 3]},'
+                    ' ${points[pIndex + 4]} ${points[pIndex + 5]}, '
+                    ' ${points[pIndex + 6]} ${points[pIndex + 7]}');
+              } else {
+                oddList.add('C ${points[pIndex + 4]} ${points[pIndex + 5]},'
+                    ' ${points[pIndex + 2]} ${points[pIndex + 3]}, '
+                    ' ${previousPoint.dx} ${previousPoint.dy} ');
+
+                previousPoint = Offset(points[pIndex + 6], points[pIndex + 7]);
+              }
+            case RositaSPath.kCloseVerb:
+              if (isEven) {
+                sb.write('Z ');
+              } else {
+                oddList.add('M ${previousPoint.dx} ${previousPoint.dy} ');
+
+                sb.writeAll(oddList.reversed);
+
+                oddList.clear();
+              }
+          }
+          if (iter.peek() != RositaSPath.kDoneVerb) {
+            sb.write(' ');
+          }
+        }
+
+        if (oddList.isNotEmpty) {
+          sb.writeAll(oddList.reversed);
+        }
+
+        final result = sb.toString();
+
+        style.setProperty('clip-path', 'path("$result")');
       }
     }
   }
