5131cdcda94c70678d7f483be83232a8513ff6a7
Egor Dolgov <contact@flutter.su>
2024-06-11 06:35:51 +0300

    [Rosita] Add RositaSkipRenderObjectMixin to RenderShiftedBox for skipping build DOM node

--- packages/flutter/lib/src/rendering/shifted_box.dart
+++ packages/flutter/lib/src/rendering/shifted_box.dart
@@ -24,7 +24,7 @@ typedef BoxConstraintsTransform = BoxConstraints Function(BoxConstraints constra
 
 /// Abstract class for one-child-layout render boxes that provide control over
 /// the child's position.
-abstract class RenderShiftedBox extends RenderBox with RenderObjectWithChildMixin<RenderBox> {
+abstract class RenderShiftedBox extends RenderBox with RenderObjectWithChildMixin<RenderBox>, RositaSkipRenderObjectMixin {
   /// Initializes the [child] property for subclasses.
   RenderShiftedBox(RenderBox? child) {
     this.child = child;
